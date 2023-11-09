3f6a964a5820dad84a31124294cfa53e12cc6d70
arsla <arsla@DESKTOP-EOES6RJ>
2023-11-09 20:50:55 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1,product2};
+
+            //type safety - tip güvenliği
+            foreach (Product product in products) 
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("------------------");
+            }
+
+            Console.WriteLine("-------------Metotlar--------------");
+
+            //instance - örnek
+            //encapsulation - kapsülleme
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,23);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12,2);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12,5);
+        }
+    }
+}
+
+//Dont repeat yourself - DRY - Clean Code - Best Practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention - isimlendirme kuralı
+        //syntax - sözdizimi
+
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Tebrikler! "+product.Adi + " sepetinize eklendi!");
+
+        }
+
+        public void Ekle2(string urunAdi,string aciklama,double fiyat,int stokAdedi) 
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : "+ urunAdi);
+        }
+    }
+}
